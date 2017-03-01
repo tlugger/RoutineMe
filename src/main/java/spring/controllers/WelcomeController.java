a03c51473ddb2d00c698ddca7e1e30ef02efe2aa
José Cañizares <jose.canizares@colorado.edu>
2017-02-28 17:24:57 -0700

    Starter Spring template with Intellij.

---
+++ src/main/java/spring/controllers/WelcomeController.java
@@ -0,0 +1,12 @@
+package spring.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(Model model) {
+        model.addAttribute("firstName", "Josess");
+        return "welcomeMessage";
+    }
+}
