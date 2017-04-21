f69cc0e033fd85fd9c7a261e19c1bab83146bcaf
user <adhe2495@colorado.edu>
2017-04-21 15:21:16 -0600

    started sorting functionaility

--- src/main/java/spring/controllers/RoutineCollection.java
+++ src/main/java/spring/controllers/RoutineCollection.java
@@ -1,16 +1,40 @@
 package spring.controllers;
 import org.springframework.stereotype.Controller;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by Nhi on 4/16/17.
  */
 @Controller
-public class RoutineCollection extends FeedItemFactory{
-    public ArrayList<Routine> collection;
-    public String collectionType;
+public class RoutineCollection { /* extends FeedItemFactory{ */
+    public List<Routine> collection;
+    public List<Routine> tempSortedCollection;
 
-    public void addRoutine(Routine routine){ }
-    public void sortByAuthor() { }
-    public void removeRoutine(Routine routine) { }
+    RoutineCollection(List<Routine> routineList){
+        this.collection = routineList;
+    }
+
+    public void addRoutine(Routine routine){
+        collection.add(routine);
+    }
+
+    public List<Routine> sortNormal() {
+        return collection;
+    }
+
+    public void sortByAuthor() {
+        Collections.sort(collection, new Comparator<Routine>() {
+            public int compare(Routine o1, Routine o2) {
+                //Sorts by 'TimeStarted' property
+                return o1.getRoutineHeader().getAuthor().compareTo(o2.getRoutineHeader().getAuthor());
+            }
+        });
+    }
+
+    public void removeRoutine(Routine routine) {
+        collection.remove(routine);
+    }
 }
