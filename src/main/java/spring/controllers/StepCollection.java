f25d623bb837c15b31bb5a58bf7ac950d97b7dbe
nhng5827 <nhng5827@colorado.edu>
2017-04-24 13:01:15 -0600

    fixed tomcat stuff

--- src/main/java/spring/controllers/StepCollection.java
+++ src/main/java/spring/controllers/StepCollection.java
@@ -11,11 +11,6 @@ public class StepCollection{
     private ArrayList<Step> collection;
 
 
-    StepCollection(){
-        Step step = new Step();
-        this.collection.add(step);
-    }
-
     public void addStep(Step step) { this.collection.add(step); }
     public void removeStep(Step step) { this.collection.remove(step);}
     public ArrayList<Step> getStepCollection() { return this.collection; }
